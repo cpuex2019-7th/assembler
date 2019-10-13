dd07e691d9653cdd463b54af5d85af80bcb18952
Takashi Yoneuchi <takashi.yoneuchi@shift-js.info>
2019-10-13 17:07:01 +0900

    enable label jumping with jal

--- cpuex_asm/asm.py
+++ cpuex_asm/asm.py
@@ -57,6 +57,7 @@ def asm_lines(lines):
                     
                     # if the current instruction is one of conditional jumps and the instruction uses a label,
                     # we have to resolve it later.
+                    # all conditional jump instructions take three ops; rs1, rs2, and rd.
                     if (instr_name in CONDITIONAL_JUMP_INSTRS \
                         and len(args) == 3 \
                         and not args[2].isdigit()):
@@ -64,10 +65,16 @@ def asm_lines(lines):
                         jumps.append((instr_name, len(instructions), target_label, line_num))
                         # 0 is temp addr
                         instructions.append(encoder[spec["type"]](spec, [args[0], args[1], 0]))
-                    # if not, we can encode the instruction usually.
+                    elif (instr_name == "jal" \
+                        and len(args) == 2 \
+                        and not args[1].isdigit()):
+                        target_label = args[1]
+                        jumps.append((instr_name, len(instructions), target_label, line_num))
+                        # 0 is temp addr
+                        instructions.append(encoder[spec["type"]](spec, [args[0], 0]))
                     elif spec["type"] in encoder:
                         instructions.append(encoder[spec["type"]](spec, args))
-                    # umm, here we got a weird metadata for the instruction ... 
+                        # umm, here we got a weird metadata for the instruction ... 
                     else:                        
                         raise Exception
                 elif instr_name in syntax_sugars and syntax_sugars[instr_name]['arg_num'] == len(args):
@@ -98,15 +105,18 @@ def asm_lines(lines):
             elif instr_name in CONDITIONAL_JUMP_INSTRS:
                 spec = instruction_specs[instr_name]                
                 instructions[i] |= encoder[spec["type"]](spec, ["x0", "x0", imm])
+            elif instr_name == "jal":
+                spec = instruction_specs[instr_name]                
+                instructions[i] |= encoder[spec["type"]](spec, ["x0", imm])
             else:
                 exit_with_error("[-] label jumping with {} is not implemented yet. sorry.".format(instr_name))
         else:
             exit_with_error("[-] invalid label found at {}: {}".format(line_num, target_label))
-    
+            
     # pack all instructions
     ################
     return b''.join([struct.pack('<I', instr) for instr in instructions])    
-        
+
 def asm_line(l):
     """
     Asm a given line and return a machine code (4 bytes).
