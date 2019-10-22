0849055bc530fa94de10e1b35dc7e3df08062ea9
Takashi Yoneuchi <takashi.yoneuchi@shift-js.info>
2019-10-23 02:50:20 +0900

    improve Makefile and make d2f remove the trailing zeros

--- tools/d2f.c
+++ tools/d2f.c
@@ -6,6 +6,8 @@ typedef union {
   int  i;
 } fi;
 
+
+
 int main(int argc, char *argv[]){
   fi f;
   if (argc < 2){
@@ -13,5 +15,11 @@ int main(int argc, char *argv[]){
   } else {
     f.i = atoi(argv[1]);  
   }
-  printf("%f", f.f);
+
+  char str[100];
+  int wrote_num = snprintf(str, 100, "%f", f.f);
+  for(int i = wrote_num - 1; str[i] == '0'; i--){
+    str[i] = '\0';
+  }
+  printf("%s", str);
 }
