ac7212bb10244338a65f9b4c103dbbf625485db6
Takashi Yoneuchi <takashi.yoneuchi@shift-js.info>
2019-10-23 02:37:33 +0900

    make d2f&f2d work with fd=0

--- tools/d2f.c
+++ tools/d2f.c
@@ -7,11 +7,11 @@ typedef union {
 } fi;
 
 int main(int argc, char *argv[]){
-  if (argc < 2){
-    printf("%s i\n", argv[0]);
-    exit(1);
-  }
   fi f;
-  f.i = atoi(argv[1]);  
+  if (argc < 2){
+    scanf("%d", &(f.i));
+  } else {
+    f.i = atoi(argv[1]);  
+  }
   printf("%f", f.f);
 }
--- tools/f2d.c
+++ tools/f2d.c
@@ -7,11 +7,11 @@ typedef union {
 } fi;
 
 int main(int argc, char *argv[]){
-  if (argc < 2){
-    printf("%s (a float ... e.g. 0.9)\n", argv[0]);
-    exit(1);
-  }
   fi f;
-  f.f = strtof(argv[1], NULL);
+  if (argc < 2){
+    scanf("%f", &(f.f));
+  } else {
+    f.f = strtof(argv[1], NULL);
+  }
   printf("%d", f.i);
 }
