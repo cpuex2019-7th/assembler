155920e97f66698aac5d3cf5a1a5a8361978a1e6
Takashi Yoneuchi <takashi.yoneuchi@shift-js.info>
2019-11-18 03:03:09 +0900

    0.0.2 -> 0.0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='cpuex_asm',
-    version="0.0.2",
+    version="0.0.3",
     author='Takashi Yoneuchi',
     author_email='takahsi.yoneuchi@shift-js.info',
     packages = ['cpuex_asm'],
